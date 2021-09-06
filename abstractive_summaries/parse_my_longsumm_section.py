9fbd3fd8310d3b73562d901e20f67b6a2721fc3b
Tomas Goldsack <tgoldsack1@gmail.com>
2021-09-06 09:23:40 +0100

    updated structure of dir

---
+++ abstractive_summaries/parse_my_longsumm_section.py
@@ -0,0 +1,117 @@
+'''
+Parses longsummm dataset json file to make format input for the given model.
+Reduces input size my using only the Abstract, Introduction and Conclusion.
+Author: Tomas Goldsack
+'''
+
+import os, json, re
+from nltk.tokenize import sent_tokenize
+from rouge_score import rouge_scorer
+
+mode = "train"
+
+longsumm_file = open(f"./my_longsumm_{mode}_abs_full.json")
+longsumm_dict = json.load(longsumm_file)
+longsumm_file.close()
+
+
+
+def assign_summ_sent(summ_sent, sections):
+  '''Assigns a summary sentence to a section of the source text'''
+  metrics = ['rouge1', 'rouge2', 'rougeL']
+  scorer = rouge_scorer.RougeScorer(metrics, use_stemmer=True)
+  
+  section_avgs = {} 
+
+  for section in sections:
+    scores = scorer.score(summ_sent.strip(), section.strip())
+    results = {}
+    results_avg = {}
+
+    for metric in metrics:
+      results[metric+"_f"].append(scores[metric].fmeasure)
+      results[metric + "_r"].append(scores[metric].recall)
+
+    for rouge_metric, rouge_scores in results.items():
+      results_avg[rouge_metric] = np.average(rouge_scores)
+
+
+
+
+## pointer-generator-pytorch-allennlp
+# Desired input format (json)
+# { 
+#   "article_lines": List[str],
+#   "summary_lines": List[str]
+# }
+
+i = 0
+for (paper_id, paper_dict) in longsumm_dict.items():
+  paper_datafile = open(f'./section_level/my_longsumm_{mode}_sections.{i}.jsonl', "w")
+
+  summary_sents = paper_dict['Y']['summary']
+  input_dict = paper_dict['X'] 
+  # X format:
+  # {
+  #   "sections": [{ "heading": str, "text": str }], 
+  #   "year": int,
+  #   "references": [{...}],
+  #   "id" : str,
+  #   "authors" : [{ "name": str, "affiliations": str }]
+  #   "abstractText": str,
+  #   "title": str
+  # }
+  has_content = False
+
+  section_data = {}
+  article_lines = []
+
+  if "abstractText" in input_dict.keys():
+    article_lines = sent_tokenize(input_dict["abstractText"])
+
+    section_data["summary_lines"] = summary_sents
+    section_data["article_lines"] = article_lines
+
+    paper_datafile.write(json.dumps(section_data))
+    paper_datafile.write("\n")
+
+    has_content = True
+
+
+  if 'sections' in input_dict.keys() and len(input_dict['sections']) > 0:
+    for section in input_dict['sections']:
+      if 'text' in section.keys():
+        section_data = {}
+        article_lines = sent_tokenize(section["text"])
+
+        if len(article_lines) > 0:
+          section_data["summary_lines"] = summary_sents
+          section_data["article_lines"] = article_lines
+
+          paper_datafile.write(json.dumps(section_data))
+          paper_datafile.write("\n")
+
+          has_content = True
+
+  if has_content:
+    i += 1
+  paper_datafile.close()
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
--- run_science_parse.sh
+++ run_science_parse.sh
@@ -4,16 +4,18 @@
 # make sure to run:
 # sudo docker run -p "0.0.0.0:8080:8080" allenai/scienceparse:2.0.3
 
-INPUT_DIR="$(pwd)/inputs/*.pdf"
+#INPUT_DIR="$(pwd)/abstractive_summaries"
+INPUT_DIR="$(pwd)/extractive_summaries"
 
+pdf_paths="$INPUT_DIR/pdf/*.pdf" 
 
-for file in ./inputs/*.pdf; do
+for file in $pdf_paths; do
   echo "-------------------------------------------"
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   echo "${filename}"
   echo "${file}"
-  output_path="./inputs/json/$filename.json"
+  output_path="$INPUT_DIR/json/$filename.json"
   echo "$output_path"
   curl -v -H "Content-type: application/pdf" --data-binary @"$file" "http://localhost:8080/v1" -o "$output_path"
 done
